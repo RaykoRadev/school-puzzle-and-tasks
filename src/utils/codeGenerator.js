0cb516d84e360afd9a0ce7447983c92b90d1ff78
RaykoRadev <christmas898@gmail.com>
2025-12-24 14:15:06 +0100

    code generator

---
+++ src/utils/codeGenerator.js
@@ -0,0 +1,19 @@
+export const generateCode = (passwordLength) => {
+    let charset = "";
+    let newPassword = "";
+
+    //todo to set up more presize after consultation
+
+    charset += "!@#$%^&*()";
+    charset += "0123456789";
+    charset += "abcdefghijklmnopqrstuvwxyz";
+    charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for (let i = 0; i < passwordLength; i++) {
+        newPassword += charset.charAt(
+            Math.floor(Math.random() * charset.length)
+        );
+    }
+
+    return newPassword;
+};
