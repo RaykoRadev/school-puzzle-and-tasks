d9949321376f2785befe46843229fe5185f39ea9
RaykoRadev <christmas898@gmail.com>
2025-12-27 15:35:46 +0100

    extracting visualizing classes name

---
+++ src/utils/visualizeClassName.js
@@ -0,0 +1,15 @@
+export default function visualizeClassName(name) {
+    let className = "";
+
+    if (name === "class1") {
+        className = "1 Клас";
+    } else if (name === "class2") {
+        className = "2 Клас";
+    } else if (name === "class3") {
+        className = "3 Клас";
+    } else if (name === "class4") {
+        className = "4 Клас";
+    }
+
+    return className;
+}
