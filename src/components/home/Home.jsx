297f7b135baf9476ed7216313aae2b539dc7ab97
RaykoRadev <christmas898@gmail.com>
2026-01-15 10:55:34 +0100

    changing  font on home screen

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,9 +4,14 @@ export default function Home() {
             <div className="max-w-screen-xl w-full">
                 <div className="max-w-4xl mx-auto text-center">
                     {/* <h1 className="text-slate-900 text-4xl md:text-5xl font-bold leading-tight"> */}
-                    <h1 className="text-slate-900 font-bold leading-tight text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
-                        Добре дошли в страничката за успешни деца на г-жа
-                        Долапчиева
+                    <h1
+                        style={{
+                            fontFamily: '"Comic Sans MS", cursive, sans-serif',
+                        }}
+                        className="ml-11 text-slate-900 font-bold leading-tight text-2xl sm:text-3xl md:text-3xl lg:text-5xl"
+                    >
+                        <p>Добре дошли в страничката за</p>
+                        <p>успешни деца на г-жа Долапчиева</p>
                     </h1>
                 </div>
             </div>
