1513434d615dc8f1526bc2c21548664f686efdd2
RaykoRadev <christmas898@gmail.com>
2026-01-15 17:43:01 +0100

    adding sorting to the student list

---
+++ src/components/teacherPage/studentsList/Sorting.jsx
@@ -0,0 +1,73 @@
+export default function Sorting({ data, sortedData }) {
+    const filters = {
+        category: "",
+        sorting: "",
+    };
+
+    //showing the selected filter for sorting
+    const handleFilterChange = (e) => {
+        const { name, value } = e.target;
+
+        filters[name] = value;
+    };
+
+    const sortHandler = (e) => {
+        const category = filters.category;
+        const sorting = filters.sorting;
+
+        let sort = 1;
+        if (sorting === "descending") {
+            sort = -1;
+        }
+
+        const sorted = data.slice().sort((a, b) => {
+            if (category === "sessions") {
+                const valA = a?.sessions?.[0] ?? "";
+                const valB = b?.sessions?.[0] ?? "";
+                return String(valA).localeCompare(String(valB)) * sort;
+            }
+            return a[category].localeCompare(b[category]) * sort;
+        });
+
+        sortedData(sorted);
+    };
+
+    return (
+        // <div className="absolute top-0 left-0 w-full p-4 bg-white shadow-md z-10">
+        <div className="absolute top-20 left-200 grid grid-cols-1 sm:grid-cols-3 gap-4">
+            {/* Selecting Category Filter */}
+
+            <select
+                id="category"
+                name="category"
+                onChange={handleFilterChange}
+                className="mt-1 block w-full text-center py-2 text-base border border-green-700 focus:outline-none focus:ring--green-700 focus:border--green-700 sm:text-sm rounded-md"
+            >
+                <option value="">Category:</option>
+                <option value="username">Name</option>
+                <option value="sessions">Last loged</option>
+                <option value="createdAt">Created at</option>
+                <option value="expiredAt">Expired at</option>
+            </select>
+
+            <select
+                id="sorting"
+                name="sorting"
+                onChange={handleFilterChange}
+                className="mt-1 block w-full text-center py-2 text-base border border-green-700 focus:outline--green-700 focus:ring--green-700 focus:border--green-700 sm:text-sm rounded-md"
+            >
+                <option value="">Sort by:</option>
+                <option value="ascending">Ascendig</option>
+                <option value="descending">Descending</option>
+            </select>
+
+            <button
+                type="button"
+                className="mt-1 block w-full py-2 text-base border border-green-700 text-center focus:outline--green-700 focus:ring--green-700 focus:border--green-700 sm:text-sm rounded-md hover:bg-green-700 hover:text-white"
+                onClick={sortHandler}
+            >
+                Sort
+            </button>
+        </div>
+    );
+}
--- src/components/teacherPage/studentsList/StudentsList.jsx
+++ src/components/teacherPage/studentsList/StudentsList.jsx
@@ -1,16 +1,28 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../../context/userContext";
 import StudentRow from "./StudentRow";
 import LogsModal from "./LogsModal";
 import { useStudentsList } from "../../../hooks/useRequestHook";
 import Spinner from "../../spinner/Spinner";
 import { toast } from "sonner";
+import Sorting from "./Sorting";
 
 export default function StudentsList() {
-    const { _id, accessToken } = useContext(UserContext);
+    const { accessToken } = useContext(UserContext);
     const [modal, setModal] = useState({ open: false, payload: null });
 
     const { data, isPending, error } = useStudentsList(accessToken);
+    const [students, setStudents] = useState(data);
+
+    useEffect(() => {
+        if (data) {
+            setStudents(data);
+        }
+    }, [data]);
+
+    const sortedData = (sorted) => {
+        setStudents(sorted);
+    };
 
     if (isPending) {
         return <Spinner />;
@@ -28,44 +40,49 @@ export default function StudentsList() {
         setModal({ open: false, payload: null });
     };
 
-    //todo sorting when the conmponent design is finalised
-
     return (
-        <div className="bg-orange-200 rounded-lg shadow-md">
-            <div className="bg-orange-200 rounded-lg shadow-md overflow-x-auto">
-                <table className="w-full text-left">
-                    <thead className="bg-orange-100">
-                        <tr>
-                            <th className="p-4">Name</th>
-                            <th className="p-4">Code</th>
-                            <th className="p-4 hidden lg:table-cell">Class</th>
-                            <th className="p-4">Last login</th>
-                            {/* <th className="p-4">visited subjects</th> */}
-                            <th className="p-4 hidden lg:table-cell">
-                                Created at:
-                            </th>
-                            <th className="p-4 hidden lg:table-cell">
-                                Expired at:
-                            </th>
-                            <th className="p-4">Controls:</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data?.map((student) => (
-                            <StudentRow
-                                key={student._id}
-                                {...student}
-                                studentId={student._id}
-                                openModal={openModal}
-                                closeModal={closeModal}
-                            />
-                        ))}
-                    </tbody>
-                </table>
+        <>
+            <div className="realtive">
+                <Sorting data={data} sortedData={sortedData} />
             </div>
-            {modal.open && (
-                <LogsModal onClose={closeModal} data={modal.payload} />
-            )}
-        </div>
+            <div className="bg-orange-200 rounded-lg shadow-md">
+                <div className="bg-orange-200 rounded-lg shadow-md overflow-x-auto">
+                    <table className="w-full text-left">
+                        <thead className="bg-orange-100">
+                            <tr>
+                                <th className="p-4">Name</th>
+                                <th className="p-4">Code</th>
+                                <th className="p-4 hidden lg:table-cell">
+                                    Class
+                                </th>
+                                <th className="p-4">Last login</th>
+                                {/* <th className="p-4">visited subjects</th> */}
+                                <th className="p-4 hidden lg:table-cell">
+                                    Created at:
+                                </th>
+                                <th className="p-4 hidden lg:table-cell">
+                                    Expired at:
+                                </th>
+                                <th className="p-4">Controls:</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {students?.map((student) => (
+                                <StudentRow
+                                    key={student._id}
+                                    {...student}
+                                    studentId={student._id}
+                                    openModal={openModal}
+                                    closeModal={closeModal}
+                                />
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                {modal.open && (
+                    <LogsModal onClose={closeModal} data={modal.payload} />
+                )}
+            </div>
+        </>
     );
 }
