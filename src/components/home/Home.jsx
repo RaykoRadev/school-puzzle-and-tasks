f28ec180e2e80c93dbbe14fbed7daa2022e528ab
RaykoRadev <christmas898@gmail.com>
2026-01-19 16:15:32 +0100

    first multi language page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,7 @@
+import { useTranslation } from "react-i18next";
+
 export default function Home() {
+    const { t } = useTranslation();
     return (
         <div className="flex-1 flex items-center justify-center px-4 sm:px-10">
             <div className="max-w-screen-xl w-full">
@@ -10,8 +13,9 @@ export default function Home() {
                         }}
                         className="text-slate-900 font-bold leading-tight text-2xl sm:text-3xl md:text-3xl md:ml-11 lg:text-5xl lg:ml-11"
                     >
-                        <p>Добре дошли в страничката за</p>
-                        <p>успешни деца на г-жа Долапчиева</p>
+                        {/* <p>Добре дошли в страничката за</p>
+                        <p>успешни деца на г-жа Долапчиева</p> */}
+                        <p>{t("welcome")}</p>
                     </h1>
                 </div>
             </div>
